3a7d05a938d9f5dde194016adc8d1e36b428a5cc
Zaprovic <jazaro2001@gmail.com>
2023-12-21 21:47:45 -0500

    finished forward method implementation

--- functions/partial/parabolic/forward.py
+++ functions/partial/parabolic/forward.py
@@ -3,11 +3,11 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 
-def forward2(f, g1, g2, xa, xb, ta, tb, h, k):
+def forward(f, alpha, g1, g2, xa, xb, ta, tb, h, k):
     N = round((xb-xa) / h)
     M = round((tb-ta) / k)
 
-    lmbda = alpha**2 * k / h**2
+    l = alpha**2 * k / h**2
     x = np.linspace(xa, xb, N+1)
     t = np.linspace(ta, tb, M+1)
     w = np.zeros((len(t), len(x)))
@@ -16,37 +16,9 @@ def forward2(f, g1, g2, xa, xb, ta, tb, h, k):
     w[1:, 0] = [g1(t[i]) for i in range(1, M+1)]
     w[1:, -1] = [g2(t[i]) for i in range(1, M+1)]
 
-    # w[1, 1] = lmbda * w[0, 2] + (1 - 2*lmbda)*w[0, 1] + lmbda*w[0, 0]
-    # w[1, 2] = lmbda * w[0, 3] + (1 - 2*lmbda)*w[0, 2] + lmbda*w[0, 1]
-    # w[1, 3] = lmbda * w[0, 4] + (1 - 2*lmbda)*w[0, 3] + lmbda*w[0, 2]
-
     for j in range(M):
-        for i in range(1, N):
-            w[j+1, i] = lmbda * w[j, i+1] + \
-                (1-2*lmbda)*w[j, i] + lmbda*w[j, i-1]
+        for i in range(1,N):
+            w[j+1, i] = l*(w[j, i+1] - 2*w[j, i] + w[j, i-1]) + w[j, i]
 
     return w
 
-
-# x interval
-xa = 0
-xb = 2
-
-# t interval
-ta = 0
-tb = 0.6
-
-alpha = 4
-h = 0.5
-k = 0.2
-
-# boundary conditions
-
-
-def f(x): return np.sin(np.pi*x/4)
-def g1(t): return 0
-def g2(t): return np.exp(-np.pi**2 * t)
-
-
-S = forward2(f, g1, g2, xa, xb, ta, tb, h, k)
-print(S)
