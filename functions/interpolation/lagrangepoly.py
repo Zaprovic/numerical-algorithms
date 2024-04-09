1510795f431bf1c840815bf5abdd2debf660d87d
Zaprovic <jazaro2001@gmail.com>
2024-04-08 19:37:09 -0500

    added Lagrange interpolation algorithm

---
+++ functions/interpolation/lagrangepoly.py
@@ -0,0 +1,58 @@
+
+import numpy as np
+
+np.set_printoptions(suppress=True)
+
+
+def lagrangepoly(x, y):
+    n = len(x)
+    y = np.array(y)
+
+    # Calculating denominators
+    d = np.prod([[x[i] - x[j] for j in range(n) if j != i]
+                for i in range(n)], axis=1)
+
+    # # denominators
+    # d = []
+    # for i in range(n):
+    #     c = 1
+    #     for j in range(n):
+    #         if i != j:
+    #             c *= (x[i] - x[j])
+    #     d.append(c)
+
+    # d = np.array(d)
+
+    # numerators
+    T = np.array([[v for idx, v in enumerate(x) if idx != i]
+                  for i in range(n)])
+
+    Tk = np.array([[[1, -i] for i in T[k]] for k in range(n)])
+
+    Lk = []
+
+    for idx, t in enumerate(Tk):
+        r = [1]
+        for seq in t:
+            r = np.convolve(r, seq)
+        Lk.append(r / d[idx])
+
+    # Lagrange coefficients
+    Lk = np.array(Lk)
+
+    c = 0
+    for i in range(n):
+        c += Lk[i]*y[i]
+
+    return c, Lk
+
+
+# Example usage
+x = [0, 3, 5, 8, 13]
+y = [0, 225, 383, 623, 993]
+
+C, L = lagrangepoly(x, y)
+
+print(C)
+print("")
+print(L)
