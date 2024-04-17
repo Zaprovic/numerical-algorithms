d918c832dc8cd35c58cf0b8015f475b7eb6ca717
Zaprovic <jazaro2001@gmail.com>
2024-04-17 12:52:43 -0500

    modified Gaussian quadrature

--- functions/integration/gauss.py
+++ functions/integration/gauss.py
@@ -3,19 +3,6 @@ from scipy.special import roots_legendre
 
 
 def gauss(f, a, b, n):
-    """
-    Parameters
-    ----------
-    f
-    a
-    b
-    n
-
-    Returns
-    -------
-
-    """
-
     x, c = roots_legendre(n)
     F = [f((b-a)*i/2 + (b+a)/2) for i in x]
     F = np.multiply(c, F)
